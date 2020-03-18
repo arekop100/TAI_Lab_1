ceb0a272b7b966c58852095a0e5ce45125a5e11e
arekop100 <arekop100@gmail.com>
2020-03-18 13:47:01 +0100

    add navbar component

---
+++ js/navbar.js
@@ -0,0 +1,18 @@
+let nav = '<nav><ul class="nav ">'+
+    '<li class="nav-item">'+
+    '<a class="nav-link active" href="index.html">Home</a>'+
+    '</li>'+
+
+    '<li class="nav-item">'+
+    '<a class="nav-link active" href="quiz.html">Quiz</a>'+
+    '</li>'+
+
+    '<li class="nav-item">'+
+    '<a class="nav-link active" href="contact.html">Kontakt</a>'+
+    '</li>'+
+    '</ul></nav>';
+
+
+let element = document.querySelector(".navbar");
+element.innerHTML = nav;
+
